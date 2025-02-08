a283ea51c2d360286caded96b134cb35d4a94063
Tachyon Tango <TachyonTango@pm.me>
2025-02-07 23:25:24 -0800

    Cleaned up main.py output report. Sorted the output of character frequencies.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-BOOK_PATH = "./books/frankenstein.txt"
+BOOK_PATH = "books/frankenstein.txt"
+
+def sort_key(dict):
+    return dict["num"]
+
+def convert_dict(dict):
+    list_of_dict = []
+    for key in dict:
+        if key.isalpha():
+            temp_dict = {"char" : key, "num" : dict[key]}
+            list_of_dict.append(temp_dict)
+    return list_of_dict
 
 def get_file(path):
     with open(BOOK_PATH) as f:
@@ -20,9 +31,19 @@ def get_char_freq(text):
 
 def main():
         book = get_file(BOOK_PATH)
-        print(book)
-        print(count_words(book))
+        word_count = count_words(book)
         char_freq = get_char_freq(book)
-        print(char_freq)
+        conv_char_freq = convert_dict(char_freq)
+        conv_char_freq.sort(reverse=True, key=sort_key)
+
+        print(f"--- Begin report of {BOOK_PATH} ---")
+        print(f"{word_count} words found in the document")
+        print("")
+        for element in conv_char_freq:
+            char = element["char"]
+            num = element["num"]
+            print(f"The '{char}' character was found {num} times")
+        print("--- End report ---")
+
 
 main()
