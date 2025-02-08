535a90f2b8c760dc24af1fcf8c70a4616b8860dc
Tachyon Tango <TachyonTango@pm.me>
2025-02-07 23:08:57 -0800

    Added get_char_freq to main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 BOOK_PATH = "./books/frankenstein.txt"
 
+def get_file(path):
+    with open(BOOK_PATH) as f:
+        return f.read()
 
 def count_words(text):
     words = text.split()
     return len(words)
 
-def get_file(path):
-    with open(BOOK_PATH) as f:
-        return f.read()
+def get_char_freq(text):
+    result_dict = {}
+    for char in text.lower():
+        if char in result_dict:
+            result_dict[char] = result_dict[char] + 1
+        else:
+            result_dict[char] = 1
+    return result_dict
+
 
 def main():
         book = get_file(BOOK_PATH)
         print(book)
         print(count_words(book))
+        char_freq = get_char_freq(book)
+        print(char_freq)
 
 main()
